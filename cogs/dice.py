2668748e331886bff9e0cde07f5087ed120e39d6
Chris <chrisboy101@gmail.com>
2020-04-15 23:12:44 -0700

    Swapped to regex based parsing with map

--- cogs/dice.py
+++ cogs/dice.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands, tasks
 import random
+import re
 
 channellist = [508905628652142592, 339235017760833536, 508225092530995220, 339155308767215618]
 rolllist = []
@@ -10,6 +11,7 @@ class diceCog(commands.Cog, name="dice"):
     def __init__(self, bot):
         self.bot = bot
         self.clear.start()
+        self.dieRegex = re.compile(r"^(?P<numDie>\d)\s?d(?P<numSides>\d)$")
 
     def dice(self, sides):
         roll = random.randint(1,sides)
@@ -38,49 +40,29 @@ class diceCog(commands.Cog, name="dice"):
                 embed.set_author(name="ullec bot")
                 embed.set_thumbnail(url=ctx.author.avatar_url)
                 try:
-                    if len(arg) > 2:
-                        raise Exception
-                    if len(arg) == 1:
-                        try:
-                            b = arg[0]
-                            b = b.strip("d")
-                            b = int(b)
-                            result = self.dice(b)
-                            rolls = 1
-                        except:
-                            b = arg[0]
-                            c = b.split("d")
-                            b = int(c[1])
-                            result = self.dice(b)
-                            rolls = int(c[0])
-                    if len(arg) == 2:
-                        for id, i in enumerate(arg):
-                            g = arg[id]
-                            if "d" in g:
-                                b = g
-                                b = b.strip("d")
-                                b = int(b)
-                                if id == 0:
-                                    rolls = int(arg[1])
-                                if id == 1:
-                                    rolls = int(arg[0])
+                    if len(arg) <= 2:
+                        result = self.dieRegex.search(" ".join(arg))
+                        numDie = result.get("numDie")
+                        numFaces = result.get("numFaces")
+                        if numDie and numFaces:
+                            if numFaces > 100:
+                                raise Exception
+                            if numDie > 14:
+                                raise Exception
 
-                            pass
-                    while rolls > r:
-                        r += 1
-                        result = self.dice(b)
-                        resultnum = resultnum + int(result)
-                        resultstring = "You rolled "+ result+"/"+str(b)
-                        output.append(resultstring)
-                    if b > 100:
-                        raise Exception
-                    if rolls > 14:
-                        raise Exception
+                            # normal way of doing it
+                            # for x in range(numDie):
+                            #    result = self.dice(x)
+
+                            # map way of doing it
+                            for result in map(self.dice, range(numDie)):
+                                resultnum += int(result)
+                                output.append("You rolled " + result + "/" + str(numFaces))
 
                     foutput = '\n'.join(output)
                     embed.insert_field_at(index=1, name=titlestring, value=foutput)
-                    print(str(resultnum) + " " + str(rolls) + " " + str(b))
-                    totalstr = "Total: " + str(resultnum) + " Average: " + str(round(int(resultnum)/int(rolls),2)) + " Percentile: " + str(int(int(resultnum) * 100 / (int(rolls) * int(b))))
+                    # print(str(resultnum) + " " + str(numDie) + " " + str(b))
+                    totalstr = "Total: " + str(resultnum) + " Average: " + str(round(int(resultnum)/int(numDie),2)) + " Percentile: " + str(int(int(resultnum) * 100 / (int(rolls) * int(b))))
                     embed.insert_field_at(index=5, name=totalstr, value="\u200b", inline=False)
                     await ctx.send(embed=embed)
                     rolllist.append(ctx.author.id)
